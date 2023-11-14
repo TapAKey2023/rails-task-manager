34e3e4c0c275e280c884573df8f6c66a0a5a6fd3
Claire Ross <cross2021@gmail.com>
2023-11-14 14:57:47 +0000

    2nd

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get '/index', to: "tasks#index"
+  get '/tasks/new', to: "tasks#new", as: :new_task
+  get '/tasks/:id', to: "tasks#details", as: :task
+  post "/tasks", to: "tasks#create"
+  get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  delete 'tasks/:id', to: "tasks#destroy", as: :destroy_task
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
